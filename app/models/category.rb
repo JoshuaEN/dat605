8c011c5a013e5d568417d7d002f4f01e940eae7c
Joshua Neuenhoff <wiirh1@live.com>
2015-08-26 08:25:14 -0400

    Fix bad validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
 	has_many :entries
 
-	validates :title, presence: true, length: { minimum: 1, maximum: 64 }
+	validates :category_title, presence: true, length: { minimum: 1, maximum: 64 }
 end
